ea8836244d690d11c4553f73aa3f20c0e00705f4
Artur Lunelli Machado <artucqc@gmail.com>
2026-06-17 20:48:41 -0300

    implementa interface web inicial

--- src/app.py
+++ src/app.py
@@ -1,13 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="../templates")
 
 @app.route("/")
 def home():
-    return """
-    <h1>ORSEGUPS Network Lab</h1>
-    <p>Automação Cisco - Projeto de Teste</p>
-    """
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
